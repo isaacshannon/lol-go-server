a978666c93322e83f8c03988cb25698fb94bea44
isaacshannon <isaac.shannon@gmail.com>
2020-02-16 19:47:28 -0500

    Mobile predictions and logging

--- app/server.go
+++ app/server.go
@@ -17,6 +17,7 @@ func main() {
 	http.HandleFunc("/predict", servePredict)
 	http.HandleFunc("/findmap", serveFindMap)
 	http.HandleFunc("/blog", serveBlog)
+	http.HandleFunc("/log", serveLog)
 
 
 	log.Println("Listening...")
@@ -24,20 +25,25 @@ func main() {
 }
 
 func serveBlog(w http.ResponseWriter, r *http.Request) {
+	log.Println("serving blog")
 	http.ServeFile(w, r, filepath.Join("templates", "blog.html"))
 }
 
 func serveHero(w http.ResponseWriter, r *http.Request) {
+	log.Println("serving landing")
 	http.ServeFile(w, r, filepath.Join("templates", "hero.html"))
 }
 
 func serveCapture(w http.ResponseWriter, r *http.Request) {
+	log.Println("serving capture")
 	http.ServeFile(w, r, filepath.Join("templates", "capture.html"))
 }
 
 func servePredict(w http.ResponseWriter, r *http.Request) {
+	log.Println("serving predict")
 	pred, err := retrievePrediction(r)
 	if err != nil {
+		log.Println(err)
 		errorResponse(w, err)
 	}
 
@@ -47,12 +53,37 @@ func servePredict(w http.ResponseWriter, r *http.Request) {
 }
 
 func serveFindMap(w http.ResponseWriter, r *http.Request) {
+	log.Println("serving find map")
 	pred, err := retrieveMap(r)
 	if err != nil {
+		log.Println(err)
 		errorResponse(w, err)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(pred)
+}
+
+func serveLog(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Each key has an array of values because each key can be declared more than once in the post form.
+	values := make(map[string]string)
+	for key, val := range r.PostForm {
+		values[key] = val[0]
+	}
+
+	marshalled, err := json.Marshal(values)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Println(string(marshalled))
+	w.WriteHeader(http.StatusOK)
 }
--- app/static/scripts/capture.js
+++ app/static/scripts/capture.js
@@ -4,6 +4,7 @@ function startUngank() {
 
     var streaming = false;
     var predicting = false;
+    var reloading = false;
 
     var video = null;
     var canvas = null;
@@ -17,44 +18,69 @@ function startUngank() {
     var mapY1 = 100;
 
     const constraints = {
+        audio: false,
         video: {
-            mediaSource: "screen", // whole screen sharing
-            width: {max: '3840'},
-            height: {max: '2160'},
-            frameRate: {max: '1'}
-        }
+            facingMode: "environment"
+        },
+        width: 600,
+        height: 600,
     };
 
-    function startPredictions(){
-        resPhoto.setAttribute('src', "../static/map-permission.png");
-        navigator.mediaDevices.getUserMedia(constraints)
-            .then(function(stream) {
-                video.srcObject = stream;
-                video.play();
-                setTimeout(function(){
-                    findMap();
-                }, 1000);
-            })
-            .catch(function(err) {
-                console.log("An error occurred: " + err);
-                resPhoto.setAttribute('src', "../static/map-error.png");
-            });
+    function handleButtonClick(ev) {
+        logToServer({"name":"capture button clicked"});
+        if (reloading) {
+            logToServer({"name":"capture button error reload"});
+            location.reload();
+        }
 
-        video.addEventListener('canplay', function(){
-            if (!streaming) {
-                height = video.videoHeight;
-                width = video.videoWidth;
-
-                video.setAttribute('width', width);
-                video.setAttribute('height', height);
-                canvas.setAttribute('width', width);
-                canvas.setAttribute('height', height);
-                streaming = true;
+        if (!streaming && !reloading) {
+            logToServer({"name":"capture button starting stream"});
+            if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
+                navigator.mediaDevices.getUserMedia(constraints).then(handleStream).catch(function (err) {
+                    logToServer(err);
+                    startbutton.innerHTML = 'reset';
+                    reloading = true;
+                });
             }
-        }, false);
+            startbutton.innerHTML = 'start predictions';
+            return;
+        }
+
+        if (streaming && !predicting) {
+            logToServer({"name":"capture button starting predictions"});
+            video.setAttribute('hidden', true);
+            resPhoto.removeAttribute("hidden");
+            predicting = true;
+            startbutton.innerHTML = 'reset';
+            findMap();
+            ev.preventDefault();
+            return;
+        }
+
+        logToServer({"name":"capture button reload"});
+        location.reload();
     }
 
-    function findMap(){
+    function handleStream(stream) {
+        video.srcObject = stream;
+        streaming = true;
+        logToServer({"name": "stream started"})
+    }
+
+    function findMap() {
+        height = video.videoHeight;
+        width = video.videoWidth;
+        logToServer({
+                "name":"finding map",
+                "width": width,
+                "height": height,
+        });
+
+        video.setAttribute('width', width);
+        video.setAttribute('height', height);
+        canvas.setAttribute('width', width);
+        canvas.setAttribute('height', height);
+
         resPhoto.setAttribute('src', "../static/map-locating.png");
         var context = canvas.getContext('2d');
         if (width && height) {
@@ -70,20 +96,36 @@ function startUngank() {
                 data: {
                     userID: userID,
                     imgBase64: canvasData
+                },
+                async: true,
+                headers: {
+                    "cache-control": "no-cache"
                 }
-            }).done(function(d) {
-                console.log(d)
+            }).done(function (d) {
                 resPhoto.setAttribute('src', d["minimap"]);
                 mapX0 = d["x0"];
                 mapX1 = d["x1"];
                 mapY0 = d["y0"];
                 mapY1 = d["y1"];
+                logToServer({
+                    "name":"map location received",
+                    "x0": d["x0"],
+                    "x1": d["x1"],
+                    "y0": d["y0"],
+                    "y1": d["y1"],
+                });
+
                 predictPositions()
             });
         }
     }
 
     function predictPositions() {
+        logToServer({
+            "name":"started prediction",
+            "width": width,
+            "height": height,
+        });
         var context = canvas.getContext('2d');
         if (width && height) {
             canvas.width = width;
@@ -91,7 +133,6 @@ function startUngank() {
             context.drawImage(video, 0, 0, width, height);
 
             var canvasData = canvas.toDataURL('image/png');
-
             $.ajax({
                 type: "POST",
                 url: "https://ungank.com/predict",
@@ -103,8 +144,8 @@ function startUngank() {
                     y0: mapY0,
                     y1: mapY1,
                 }
-            }).done(function(d) {
-                console.log(d);
+            }).done(function (d) {
+                logToServer("prediction received");
                 resPhoto.setAttribute('src', d["result"]);
                 if (predicting) {
                     predictPositions();
@@ -113,20 +154,20 @@ function startUngank() {
         }
     }
 
-    console.log("starting up")
+    function logToServer(event) {
+        $.ajax({
+            type: "POST",
+            url: "https://ungank.com/log",
+            data: event,
+            async: true,
+        })
+    }
+
+    console.log("starting up");
     video = document.getElementById('video');
     canvas = document.getElementById('canvas');
     resPhoto = document.getElementById('response');
     startbutton = document.getElementById('startbutton');
 
-    startbutton.addEventListener('click', function(ev){
-        if (predicting) {
-            location.reload();
-        } else {
-            predicting = true;
-            startbutton.innerHTML = 'stop';
-            startPredictions();
-            ev.preventDefault();
-        }
-    }, false);
+    startbutton.addEventListener('click', handleButtonClick, false);
 }
